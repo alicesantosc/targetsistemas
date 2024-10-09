38464a6a21a756973393f6536ec8f18f512d85c8
Alice Almeida <165727441+alicesantosc@users.noreply.github.com>
2024-10-09 17:11:54 -0300

    Update Logica.java

--- src/vaga/Logica.java
+++ src/vaga/Logica.java
@@ -1,5 +1,13 @@
 package vaga;
 
 public class Logica {
-
+  ///Para descobrir o próximo elemento:
+	///a) 1,3,5,7,9 -> soma 2 ao antecessor
+	///b) 2,4,8,16,32,64 -> multiplica por 2 o antecessor
+	///c) 0,1,4,9,16,25,36,49 -> somar o antecessor pelo numero ímpar correspondente(1,3,5,7,9,11,13) com o mesmo índice na lista, considerando-se número ímpares positivos
+	///d) 4,16,36,64,100-> o quadrado de números pares, começando pelo 2
+	///e) 1,1,2,3,5,8,13 -> sequência de fibonacci, antecessor somado ao número que o antecede
+	///f) 2,10,12,16,17,18,19,200 -> número inteiro que em português é escrito com D
 }
+
+
